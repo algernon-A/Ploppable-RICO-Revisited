4918017ae05b4bbc80eda51a1ad608a21b8b5069
algernon-A <aeneas@aloku.net>
2021-10-16 18:37:41 +1100

    Change too few services complaint disabling from LevelUpExtension to Harmony Postfix.

--- PloppableRCI/Code/Utils/RICOUtils.cs
+++ PloppableRCI/Code/Utils/RICOUtils.cs
@@ -1,4 +1,7 @@
-﻿namespace PloppableRICO
+﻿using ColossalFramework;
+
+
+namespace PloppableRICO
 {
     /// <summary>
     /// Internal static RICO utilities class.
@@ -19,6 +22,22 @@
         /// <param name="buildingID">Building instance ID</param>
         /// <returns>True if this building has a Ploppable RICO non-growable custom AI, false otherwise</returns>
         internal static bool IsRICOPloppableAI(PrivateBuildingAI prefabAI) => prefabAI != null && (prefabAI is PloppableResidentialAI || prefabAI is PloppableCommercialAI || prefabAI is PloppableIndustrialAI || prefabAI is PloppableOfficeAI || prefabAI is PloppableExtractorAI);
+
+
+        /// <summary>
+        /// Checks to see whether or not the specified building is a Ploppable RICO building.
+        /// </summary>
+        /// <param name="buildingID">Building instance ID</param>
+        /// <returns>True if this is a Ploppable RICO building, false otherwise</returns>
+        internal static bool IsRICOBuilding(ushort buildingID) => IsRICOAI(Singleton<BuildingManager>.instance.m_buildings.m_buffer[buildingID].Info.GetAI() as PrivateBuildingAI);
+
+
+        /// <summary>
+        /// Checks to see whether or not the specified building is a Ploppable RICO non-growable building.
+        /// </summary>
+        /// <param name="buildingID">Building instance ID</param>
+        /// <returns>True if this is a Ploppable RICO non-growable building, false otherwise</returns>
+        internal static bool IsRICOPloppable(ushort buildingID) => IsRICOPloppableAI(Singleton<BuildingManager>.instance.m_buildings.m_buffer[buildingID].Info.GetAI() as PrivateBuildingAI);
 
 
         /// <summary>
