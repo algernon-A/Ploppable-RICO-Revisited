aa63f6cdfd382c55f8bd9ed07524117d47664f85
algernon-A <aeneas@aloku.net>
2022-02-18 22:33:36 +1100

    Add option to disable style despawning.

---
+++ PloppableRCI/Code/Patches/PrivateBuildingSimStep.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+
+
+namespace PloppableRICO
+{
+    [HarmonyPatch(typeof(PrivateBuildingAI), nameof(PrivateBuildingAI.SimulationStep))]
+    public static class PrivateBuildingSimStep
+    {
+        public static bool disableStyleDespawn = false;
+
+        /// <summary>
+        /// Harmony transpiler to disable despawning due to district style mismatch if relevant mod option is set.
+        /// </summary>
+        /// <param name="instructions">CIL code to alter.</param>
+        /// <returns></returns>
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            // Instruction parsing.
+            IEnumerator<CodeInstruction> instructionsEnumerator = instructions.GetEnumerator();
+            CodeInstruction instruction;
+            //Label targetLabel = generator.DefineLabel();
+            bool inserted = false;
+
+            Logging.KeyMessage("transpiling PrivateBuildingAI.SimulationStep");
+
+            // Iterate through all instructions in original method.
+            while (instructionsEnumerator.MoveNext())
+            {
+                // Get next instruction and add it to output.
+                instruction = instructionsEnumerator.Current;
+                yield return instruction;
+
+                // Look for first (and only) ble in code.
+                if (!inserted && instruction.opcode == OpCodes.Ble)
+                {
+                    // Found it - reflect disableStyleDespawn field.
+                    FieldInfo disableField = typeof(PrivateBuildingSimStep).GetField(nameof(disableStyleDespawn), BindingFlags.Static | BindingFlags.Public);
+                    if (disableField == null)
+                    {
+                        Logging.Error("couldn't reflect disableStyleDespawn");
+                    }
+
+                    // Insert check to our status override (if disableStyleDespawn...).
+                    yield return new CodeInstruction(OpCodes.Ldsfld, disableField);
+                    yield return new CodeInstruction(OpCodes.Brtrue, instruction.operand);
+
+                    // Set flag.
+                    inserted = true;
+                }
+            }
+        }
+    }
+}
